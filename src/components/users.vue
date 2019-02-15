059197a92ee3bde66eb9641b64c319d72c9ff814
zyb <zyb@example.com>
2019-02-15 16:17:51 +0800

    users页面创建

--- src/components/users.vue
+++ src/components/users.vue
@@ -1,9 +1,11 @@
 <template>
+ <el-card>
   <el-breadcrumb separator-class="el-icon-arrow-right">
-  <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-  <el-breadcrumb-item>用户管理</el-breadcrumb-item>
-  <el-breadcrumb-item>用户列表</el-breadcrumb-item>
-</el-breadcrumb>
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>用户管理</el-breadcrumb-item>
+      <el-breadcrumb-item>用户列表</el-breadcrumb-item>
+  </el-breadcrumb>
+ </el-card>
 </template>
 
 <script>
--- src/router/index.js
+++ src/router/index.js
@@ -2,19 +2,27 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import home from '@/components/home.vue'
 import login from '@/components/login.vue'
+import users from '@/components/users.vue'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-       name:'home',
-       path:'/',
-       component:home,
+      name: "home",
+      path: "/",
+      component: home,
+      children: [
+        {
+          name: "users",
+          path: "/users", // <el-menu-item index="users">这里path值得index的值保持一致
+          component: users
+        }
+      ]
     },
     {
-      name:'login',
-      path:'/login',
-      component:login
-   }
+      name: "login",
+      path: "/login",
+      component: login
+    }
   ]
-})
+});
